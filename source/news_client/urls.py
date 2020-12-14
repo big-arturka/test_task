3f0e1dc2c4014677ff8d681ab4aef1704a7b8221
big-smoke <arturkrmnlv10@gmail.com>
2020-12-14 22:05:38 +0600

    Созданы представления для просмотра, создания, редактирования и удаления статьи. Добавлена модельная форма для статьи. Добавлены html-шаблоны. Прописаны url-паттеры.

--- source/news_client/urls.py
+++ source/news_client/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from django.urls import path, include
 
-from news_client.views import IndexView
+from news_client.views import IndexView, ArticleView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'news_client'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+
+    path('article/', include([
+            path('<int:pk>/', include([
+                path('', ArticleView.as_view(), name='article_view'),
+                path('update/', ArticleUpdateView.as_view(), name='article_update'),
+                path('delete/', ArticleDeleteView.as_view(), name='article_delete'),
+            ])),
+
+            path('add/', ArticleCreateView.as_view(), name='article_create'),
+        ])),
 ]
--- source/news_client/views/article_views.py
+++ source/news_client/views/article_views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+
+from news_client.forms import ArticleForm
 from news_client.models import Article
 
 
@@ -11,3 +14,27 @@ class IndexView(ListView):
     def get_queryset(self):
         return Article.objects.all()
 
+
+class ArticleView(DetailView):
+    template_name = 'article/article_view.html'
+    model = Article
+    context_object_name = 'article'
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'article/article_create.html'
+    form_class = ArticleForm
+    model = Article
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'article/article_update.html'
+    form_class = ArticleForm
+    model = Article
+    permission_required = 'webapp.change_article'
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'article/article_delete.html'
+    model = Article
+    success_url = reverse_lazy('webapp:index')
