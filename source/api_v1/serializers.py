594c3469bd64a47519964ad3bfead9ac86d95bed
big-smoke <arturkrmnlv10@gmail.com>
2020-12-18 01:52:53 +0600

    Дополнены сериализаторы, добавлен вывод имени автора статьи и название категории, при запросе на точку входа получения списка статей.

--- source/api_v1/serializers.py
+++ source/api_v1/serializers.py
@@ -1,14 +1,24 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from news_client.models import Article, Category
-
-
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ['id', 'category_id', 'title', 'description', 'user_id']
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['title', 'parent_id']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    category_name = CategorySerializer(read_only=True, source='category_id')
+    author_name = UserSerializer(read_only=True, source='user_id')
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'description', 'category_id', 'category_name', 'user_id', 'author_name']
